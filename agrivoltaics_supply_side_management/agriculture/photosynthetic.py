Calculation of crop yield: Fixed calculation and in unit test, fixed errors in parameter values and converted the first test to use @pytest.mark.parametrize.

agrivoltaics_supply_side_management/agriculture/photosynthetic.py
@@ -9,9 +9,13 @@ class PhotosyntheticModel:
         coefficient_1 = 0.001
         coefficient_2 = -0.65
 
-        crop_yield = (harvest_index * (coefficient_1 * number_of_days) * biomass_energy_ratio
+        # TODO: Modified to make the sum over list of
+        #       photosynthetically_active_radiation
+        #       except for harvest_index, which is outside of summation
+        crop_yield = (harvest_index * coefficient_1 * biomass_energy_ratio
                       * photosynthetically_active_radiation
                       * (1 - np.exp(coefficient_2 * leaf_area_index))
-                      * crop_growth_regulating_factor)
+                      * crop_growth_regulating_factor
+                      * number_of_days)
 
         return crop_yield
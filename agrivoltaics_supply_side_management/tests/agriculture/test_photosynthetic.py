4d181b43ef5749fea02ae21ef4d91008f432a752
tadatoshi <tadatoshi@gmail.com>
2022-07-13 17:23:01 -0700

    For crop yield calculation: Converted the second unit test to use Hypothesis @given for all the parameters except for photosynthetically_active_radiation.

--- agrivoltaics_supply_side_management/tests/agriculture/test_photosynthetic.py
+++ agrivoltaics_supply_side_management/tests/agriculture/test_photosynthetic.py
@@ -25,18 +25,26 @@ class TestPhotosyntheticModel:
         “Optimisation of vertically mounted agrivoltaic systems,”
         Journal of Cleaner Production, vol. 325, p. 129091, Nov. 2021,
         doi: 10.1016/j.jclepro.2021.129091.
+        TODO: biomass_energy_ratio has kg in unit and crop_yield has ton
+        in unit. Thus, the units don't match. Investigate.
 
         Arguments
         ---------
         photosynthetically_active_radiation : float (will be changed to list)
-            based on 5 peak sun hours
+            [MJ/m^2]
+            Based on 5 peak sun hours
             with solar irradiance 1000[W/m^2] per day on a sunnay summer day
             and [MJ] = [Wh] * 60[min/h] * 60[sec/min] / 1000000
             1000[W/m^2] -> 5000[Wh/m^2] -> 18[MJ/m^2]
         number_of_days: integer
             4 momths between planting and harvesting
             with average 30 days / month
+        expected_crop_yield: float [ton/ha]
+
         """
+
+        # TODO: biomass_energy_ratio has kg in unit and crop_yield has ton
+        #       in unit. Thus, the units don't match. Investigate.
 
         # harvest_index = 0.95
         # biomass_energy_ratio = 30
@@ -53,17 +61,15 @@ class TestPhotosyntheticModel:
 
         assert actual_crop_yield == pytest.approx(expected_crop_yield, 1)
 
-    @given(floats(0, 1))
-    def test_crop_yield_potato_with_property(self, harvest_index):
+    @given(floats(0.1, 1), floats(20, 40), floats(1, 10), floats(0.1, 1),
+           floats(2.5 * 30, 6 * 30))
+    def test_crop_yield_with_property(self, harvest_index,
+        biomass_energy_ratio, leaf_area_index, crop_growth_regulating_factor,
+        number_of_days):
         # assume(harvest_index >= 0 and harvest_index <=1)
 
-        biomass_energy_ratio = 30
         #  [MJ] = [Wh] * 60[min/h] * 60[sec/min] / 1000000
         photosynthetically_active_radiation = 5000 * 60 * 60 / 1000000
-        leaf_area_index = 5
-        crop_growth_regulating_factor = 0.95
-        # 4 momths between planting and harvesting
-        number_of_days = 4 * 30
 
         photosynthetic_model = PhotosyntheticModel()
 
@@ -71,11 +77,11 @@ class TestPhotosyntheticModel:
                    photosynthetically_active_radiation,
                    leaf_area_index, crop_growth_regulating_factor, number_of_days)
 
-        #print("actual_crop_yield:", actual_crop_yield)
+        # print("actual_crop_yield:", actual_crop_yield)
 
-        # Good harvest is 25 tons per hectare and experienced farmers can
-        # achieve 40 to 70 tons per hectare
+        # For pootatos, Good harvest is 25 tons per hectare and experienced farmers
+        # can achieve 40 to 70 tons per hectare
         # per year with 2.5 to 4 momths between planting and harvesting
         # accordiing to https://wikifarmer.com/potato-harvest-yield-storage/
-        assert actual_crop_yield <=70
-        assert actual_crop_yield >=0
+        assert actual_crop_yield <= 130
+        assert actual_crop_yield >= 0

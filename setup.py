86906cc33203f3ac3a78d8e9727ec5b5b119cc9f
tadatoshi <tadatoshi@gmail.com>
2022-08-01 20:36:49 -0700

    Added a notebook for performing regression on net photosynthetic rate.

--- setup.py
+++ setup.py
@@ -145,8 +145,9 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     #install_requires=['peppercorn'],  # Optional
-    install_requires=['numpy', 'pandas', 'matplotlib', 'seaborn', 'scikit-learn', 'statsmodels',
-                      'pvlib', 'opencv-python', 'pyomo', 'jupyterlab'],
+    install_requires=['numpy', 'pandas', 'matplotlib', 'seaborn', 'scikit-learn',
+                      'statsmodels', 'tensorflow', 'tensorflow-probability',
+                      'pymc3', 'pvlib', 'opencv-python', 'pyomo', 'jupyterlab'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"

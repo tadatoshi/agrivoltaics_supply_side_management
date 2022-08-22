e01da2b251b49df8738d61eee2e965aba6168225
tadatoshi <tadatoshi@gmail.com>
2022-08-22 16:48:54 -0700

    Corrected PV module's mazimum power: from per module to per m^2.

--- agrivoltaics_supply_side_management/photovoltaics/pv_modules.py
+++ agrivoltaics_supply_side_management/photovoltaics/pv_modules.py
@@ -4,10 +4,10 @@
 class ElectricityGeneration:
 
     def __init__(self):
-        # Default value. Changes depending on specific module
+        # Default value. [W/m^2] Changes depending on specific module
         # It's for standard test condition (STD) of irradiance 1000[W/m^2],
         # 25[degree-C], and Air Mass 1.5
-        self._module_mpp = 450
+        self._module_mpp = 210
 
     def consume_light_power(self, irradiance):
         assert irradiance >= 0
@@ -18,9 +18,7 @@ class ElectricityGeneration:
         if self._irradiance <= 1000:
             # Simple way assuming that module power changes linear to
             # irradiance with 0 as intercept.
-            # It's true for current but not for voltage, which has
-            # a different value as intercept.
             # For accuracy, needs to refer to module's datasheet.
-            return 450 * (self._irradiance / 1000)
+            return self._module_mpp * (self._irradiance / 1000)
         else:
-            return 450
+            return self._module_mpp

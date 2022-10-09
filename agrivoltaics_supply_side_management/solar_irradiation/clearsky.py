aa3e7c4c2ba6bfd0c50900c74b7a85b89293223e
tadatoshi <tadatoshi@gmail.com>
2022-10-08 18:33:52 -0700

    Added a Jupyter notebook for exploratory data analysis for solar resource time series.

--- agrivoltaics_supply_side_management/solar_irradiation/clearsky.py
+++ agrivoltaics_supply_side_management/solar_irradiation/clearsky.py
@@ -1,7 +1,9 @@
 from pvlib import location
 
 
-def get_clearsky(lattitude, longitude, timezone, times):
-    site_location = location.Location(lattitude, longitude, tz=timezone, name='Vancouver, BC, Canada')
-    solar_position = site_location.get_solarposition(times)
-    return site_location.get_clearsky(times)
+def get_clearsky(lattitude, longitude, timezone, time_range,
+                 location_name='Vancouver, BC, Canada'):
+    site_location = location.Location(lattitude, longitude, tz=timezone,
+                                      name=location_name)
+    solar_position = site_location.get_solarposition(time_range)
+    return site_location.get_clearsky(time_range)

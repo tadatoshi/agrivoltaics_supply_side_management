9a02007ad68d28269ac2ae540043dfc87274825f
tadatoshi <tadatoshi@gmail.com>
2022-11-21 19:20:54 -0800

    Added Jupyter notebook for trying fixed-tilt bifacial module modeling by pvlib-python.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',  # Required
+    version='0.1.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -147,7 +147,7 @@ setup(
     #install_requires=['peppercorn'],  # Optional
     install_requires=['numpy', 'pandas', 'matplotlib', 'seaborn', 'scikit-learn',
                       'statsmodels', 'tensorflow', 'tensorflow-probability',
-                      'pymc3', 'pvlib', 'opencv-python', 'pyomo',
+                      'pymc3', 'pvlib', 'pvfactors', 'opencv-python', 'pyomo',
                       'kneed', 'jupyterlab'],
 
     # List additional groups of dependencies here (e.g. development

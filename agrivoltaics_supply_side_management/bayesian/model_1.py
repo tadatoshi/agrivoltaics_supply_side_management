a05e02be2a930bbcc023bcfb474e260ac7283a24
tadatoshi <tadatoshi@gmail.com>
2023-01-12 21:28:43 -0800

    Extracted Bayesian modeling code from Jupyter notebook to agrivoltaics_supply_side_management package code. .

---
+++ agrivoltaics_supply_side_management/bayesian/model_1.py
@@ -0,0 +1,28 @@
+import pymc3 as pm
+
+
+class BayesianModel1:
+
+    def execute(self, std_net_photosynthetic_rate,
+                prior_mean_phi, prior_mean_alpha, prior_mean_theta, ppfd,
+                net_photosynthetic_rate_data,
+                net_photosynthetic_rate_function):
+
+        with pm.Model() as model_1:
+            std = pm.HalfStudentT('sigma', std_net_photosynthetic_rate, 100)
+            phi = pm.Normal('phi', prior_mean_phi, 1.0)
+            alpha = pm.Normal('alpha', prior_mean_alpha, 1.0)
+            theta = pm.Normal('theta', prior_mean_theta, 10.0)
+            mu = pm.Deterministic('mu',
+                    net_photosynthetic_rate_function(ppfd, phi, alpha, theta))
+
+            net_photosynthetic_rate = pm.Normal('net_photosynthetic_rate',
+                    mu=mu, sigma=std, observed=net_photosynthetic_rate_data)
+
+            idata = pm.sample(3000, tune=2000, return_inferencedata=True)
+
+        posterior_mean_phi = idata['posterior']['phi'].to_numpy().mean()
+        posterior_mean_alpha = idata['posterior']['alpha'].to_numpy().mean()
+        posterior_mean_theta = idata['posterior']['theta'].to_numpy().mean()
+
+        return posterior_mean_phi, posterior_mean_alpha, posterior_mean_theta
---
+++ agrivoltaics_supply_side_management/tests/bayesian/test_model_1.py
@@ -0,0 +1,53 @@
+import pytest
+import numpy as np
+import pandas as pd
+
+from agrivoltaics_supply_side_management.agriculture.photosynthetic_rate\
+    import PhotosyntheticRate
+from agrivoltaics_supply_side_management.bayesian.model_1\
+    import BayesianModel1
+
+
+class TestModel1:
+
+    @pytest.fixture()
+    def net_photosynthetic_rate_df(self):
+        index_column_name = "Photosynthetically Active Radiation" \
+                            + " [micromol / m^2 / s]"
+        column_1_name = 'Monocropping - CO2 uptake [micromol / m^2 / s]'
+        column_2_name = 'Intercropping - CO2 uptake [micromol / m^2 / s]'
+        data_df = pd.read_csv("data/soybean_net_photosynthetic_rate.csv",
+                              index_col=index_column_name)
+        data_df.rename(columns={column_1_name: 'Monocropping',
+                                column_2_name: 'Intercropping'},
+                       inplace=True)
+        data_df.index.names = ['PAR']
+        return data_df
+
+    def test_execute(self, net_photosynthetic_rate_df):
+
+        ppfd = net_photosynthetic_rate_df.index.values
+        net_photosynthetic_rate_data \
+            = net_photosynthetic_rate_df.Intercropping.values
+        p_max = max(net_photosynthetic_rate_data)
+        net_photosynthetic_rate_function = \
+            PhotosyntheticRate.get_net_photosynthetic_rate_function(
+                p_max, avoid_nan=False)
+        prior_mean_phi, prior_mean_alpha, prior_mean_theta \
+            = PhotosyntheticRate.net_photosynthetic_rate_parameters(ppfd,
+                        net_photosynthetic_rate_data)
+
+        std_net_photosynthetic_rate \
+            = net_photosynthetic_rate_df.Intercropping.std()
+
+        bayesian_model_1 = BayesianModel1()
+        actual_posterior_mean_phi, actual_posterior_mean_alpha, \
+        actual_posterior_mean_theta \
+            = bayesian_model_1.execute(std_net_photosynthetic_rate,
+                prior_mean_phi, prior_mean_alpha, prior_mean_theta, ppfd,
+                net_photosynthetic_rate_data,
+                net_photosynthetic_rate_function)
+
+        assert 0.01 < actual_posterior_mean_phi < 1.0
+        assert 0.01 < actual_posterior_mean_alpha < 1.0
+        assert -4.0 < actual_posterior_mean_theta < 4.0
